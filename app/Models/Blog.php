344f55ac08af351015378c352990eb8e54dd190b
Wahyu <wahyuivan999@ymail.com>
2021-12-10 13:35:40 +0800

    feat(Add): add models

---
+++ app/Models/Blog.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id', 
+        'blog_category_id',
+        'title',
+        'content',
+        'image',
+        'view_count',
+        'meta_title',
+        'meta_description',
+        'slug',
+        'published_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function blogCategory()
+    {
+        return $this->belongsTo(BlogCategories::class);
+    }
+}
