a06401d9f6b3756e488e17e9be26a7fb82e08cdc
adyahoo <60729024+adyahoo@users.noreply.github.com>
2021-12-13 11:58:14 +0800

    feat(add): home blog page no responsive

---
+++ resources/js/blog/swiper-js.js
@@ -0,0 +1,22 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    
+      // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+      // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
