59b765caa4b421d6c119c659a271f18962fa6e7a
Wahyu <wahyuivan999@ymail.com>
2021-12-17 13:09:00 +0800

    feat(Add): add global log

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,11 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+use Auth;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
     protected $fillable = ['name','privilege'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+                        ->useLogName('role')
+                        ->logOnly(['name','privilege']);
+    }
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "Role has been {$eventName} by ". Auth::user()->name;
+    }
 
     public function user()
     {
---
+++ resources/views/project/admin/pages/projects/version/detail.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.project_manager')
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('templates/stisla/node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('templates/stisla/node_modules/datatables.net-select-bs4/css/select.bootstrap4.min.css') }}">
+@endsection
+
+@section('content')
+    <div class="section-header" style="display: block">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-12 col-sm-12">
+                <h1>{{ $version->project->name }}</h1>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 col-sm-12 text-right">
+                <p>Latest Version v{{ $latestVersion->version_number }}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-12 col-sm-12">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('project_manager.projects.detail', $version->project) }}">Detail</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('project_manager.projects.module.all', $version->project) }}">Modules</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active-tab" href="{{ route('project_manager.projects.version.all', $version->project) }}">Version</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logs</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12 col-sm-12 text-right">
+                <a href="#" class="btn btn-icon btn-primary"><i class="fa fa-cog"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-dark">{{ $version->version_number }}</h2>
+                    <p>{{ $version->description }}</p>
+                    <hr>
+                    <p>{{ $version->note }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script src="{{ asset('templates/stisla/node_modules/datatables/media/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('templates/stisla/node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('templates/stisla/node_modules/datatables.net-select-bs4/js/select.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('templates/stisla/node_modules/sweetalert/dist/sweetalert.min.js') }}"></script>
+    @if (Session::has('success'))
+        <script>
+            swal("Success!", "{{ Session::get('success') }}", "success");
+        </script>
+    @endif
+    @if($errors->any())
+        <script>
+            var msg = "{{ implode(' \n', $errors->all(':message')) }}";
+            swal("Error!", msg , "error");
+        </script>
+    @endif
+    <script>
+        window.deleteConfirm = function(formId) {
+            swal({
+                title: 'Delete Confirmation',
+                icon: 'warning',
+                text: 'Do you want to delete this?',
+                buttons: true,
+                dangerMode: true,
+            }).then((willDelete) => {
+                if (willDelete) {
+                    $('#'+formId).submit();
+                }
+            });
+        }
+    </script>
+    <script>
+        $("#table-1").dataTable({
+            
+        });
+    </script>
+    <script>
+        $(".btn-add").click(function(){
+            let action = $(this).data('action');
+            $('#title').text('Add Role')
+            $('#form').attr('action', action);
+            $("#form").attr("method", "post");
+        });
+
+        $(".btn-edit").click(function(){
+            let action = $(this).data('action');
+            let detail = $(this).data('detail');
+            $('#title').text('Edit Role')
+            $('#form').attr('action', action);
+            $("#form").attr("method", "post");
+            $("#method").attr("value", "put");
+            $.get(detail, function (data) {
+                $('#roleName').val(data.name);
+                $('#privilege').val(data.privilege);
+            });
+        });
+    </script>
+@endsection
